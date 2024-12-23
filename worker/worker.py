7c7632de183fa9dcc8623a973eddab4758d6f7ec
aleksandr <c.akst@mail.ru>
2024-12-23 19:27:10 +0300

    update worker container

--- worker/worker.py
+++ worker/worker.py
@@ -3,4 +3,4 @@ import time
 print("Worker service is running...")
 while True:
     time.sleep(60)  # Задача выполняется каждые 60 секунд
-    print("Performing background tasks...")а 
+    print("Performing background tasks...")
